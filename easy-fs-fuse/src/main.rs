e57a3b4dc731ece998760f4b67516fb7dcff5efa
chyyuu <yuchen@tsinghua.edu.cn>
2024-05-02 21:45:16 +0800

    remove easy-fs DIR, but add easyfs crate in crates.io

--- easy-fs-fuse/src/main.rs
+++ easy-fs-fuse/src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, Arg};
-use easy_fs::{BlockDevice, EasyFileSystem};
+use easyfs::{BlockDevice, EasyFileSystem};
 use std::fs::{read_dir, File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::sync::Arc;
